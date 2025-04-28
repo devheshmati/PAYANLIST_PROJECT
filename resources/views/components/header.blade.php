f0fcd3c1d4bc8b5eaf1c28ecf07257c5fe13b4c3
devheshmati <reza2boyce@gmail.com>
2025-04-28 20:11:51 +0330

    closes #5, icon created and applied

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,9 +1,10 @@
 <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" class="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <a href="https://payanlist.ir" class="flex items-center justify-center gap-1">
+                <img src="/images/payanlist-logo-finish.svg" class="w-[20px] h-[20px]" alt="Payanlist Logo">
+                <span
+                    class="self-center text-xl font-semibold whitespace-nowrap dark:text-white mb-[-4px]">PAYANLIST</span>
             </a>
             <div class="flex items-center lg:order-2">
                 <a href="#"

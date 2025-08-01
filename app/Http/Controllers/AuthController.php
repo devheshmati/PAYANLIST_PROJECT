9a0ff5d0402cae6039f707d86daf00083f931fda
devheshmati <reza2boyce@gmail.com>
2025-08-02 02:24:12 +0330

    update login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,21 +11,23 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    # Web Authentication
+    // Web Authentication
     // login
     public function login(Request $request)
     {
-        $validator = $request->validate([
+        $validator = $request->validate(
+            [
             'email' => 'required|email|exists:users,email',
             'password' => 'required'
-        ]);
+            ]
+        );
 
         $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('user/dashboard');
+            return redirect()->route('user.dashboard');
         }
 
         return back()->withErrors($validator);
@@ -39,11 +41,13 @@ class AuthController extends Controller
     // register
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $validator = Validator::make(
+            $request->all(), [
             'phone_number' => 'required|string|max:11|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => "required|string|min:8|max:255|confirmed"
-        ]);
+            ]
+        );
 
         if ($validator->fails()) {
             return redirect('/auth/register')
@@ -51,11 +55,13 @@ class AuthController extends Controller
                 ->withInput();
         }
 
-        $user = User::create([
+        $user = User::create(
+            [
             'phone_number' => $request->phone_number,
             'email' => $request->email,
             'password' => Hash::make($request->password)
-        ]);
+            ]
+        );
 
         session()->flash('redirect-after-register-message', 'true');
 
@@ -70,13 +76,17 @@ class AuthController extends Controller
     }
 
     // forget password
-    public function forgetPassword() {}
+    public function forgetPassword()
+    {
+    }
     public function showForgetPassword()
     {
         return view('auth.forget-password');
     }
 
-    public function resetPassword() {}
+    public function resetPassword()
+    {
+    }
     public function showResetPassword()
     {
         return view('auth.reset-password');
@@ -92,9 +102,17 @@ class AuthController extends Controller
         return redirect()->route('home');
     }
 
-    # API Authentication
-    public function apiLogin() {}
-    public function apiRegister() {}
-    public function apiForgetPassword() {}
-    public function apiLogout() {}
+    // API Authentication
+    public function apiLogin()
+    {
+    }
+    public function apiRegister()
+    {
+    }
+    public function apiForgetPassword()
+    {
+    }
+    public function apiLogout()
+    {
+    }
 }
